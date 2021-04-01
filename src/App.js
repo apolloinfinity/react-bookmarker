e2e42cfba211dc6c203b2c27e6ef7aadbd66166a
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2021-04-01 13:01:28 -0700

    Created an object for the button styles that will be used with the Button component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 // Components
 import Header from './components/Header';
 import Form from './components/Form';
-import List from './components/List';
+import BookmarkList from './components/BookmarkList';
 
 function App() {
   // Functions and properties for components here
+  const buttonStyle = {
+    primary: 'btn-primary',
+    secondary: 'btn-secondary',
+    danger: 'btn-danger',
+  };
   return (
     <div className='App container'>
+      <Header />
       <div className='row bg-white rounded mt-5 shadow-sm p-4'>
-        <Header />
-        <Form />
+        <Form buttonStyle={buttonStyle} />
       </div>
-      <List />
+      <BookmarkList buttonStyle={buttonStyle} />
     </div>
   );
 }
