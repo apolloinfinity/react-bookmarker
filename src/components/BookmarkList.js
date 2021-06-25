42a16161a814758b1b1a2fbeb2dad53310d79248
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2021-06-25 14:38:08 -0700

    Bookmark list takes some props so that data can be passed between child to parent. Most notable is the onDelete

--- src/components/BookmarkList.js
+++ src/components/BookmarkList.js
@@ -1,12 +1,21 @@
 import Bookmark from './Bookmark';
 
-const BookmarkList = ({ buttonStyle, bookmarks }) => {
+const BookmarkList = ({ buttonStyle, bookmarks, deleteBookmarkHandler }) => {
 	return (
 		<div className='row bg-white rounded mt-5 shadow-sm p-4'>
 			<ul className='list-group'>
-				{bookmarks.map((bookmark, index) => (
-					<Bookmark key={index} bookmark={bookmark} buttonStyle={buttonStyle} />
-				))}
+				{bookmarks.length > 0 ? (
+					bookmarks.map((bookmark) => (
+						<Bookmark
+							key={bookmark._id}
+							bookmark={bookmark}
+							buttonStyle={buttonStyle}
+							onDelete={deleteBookmarkHandler}
+						/>
+					))
+				) : (
+					<p>No Bookmarks</p>
+				)}
 			</ul>
 		</div>
 	);
